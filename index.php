e4d6a6b4ca8d874bf5b5954b5cb425a59e0f83c5
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2021-01-24 17:19:58 +0600

    project set up

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Multivendor Ecommerce Site</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	 
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/fontawesome.min.css"/>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+
+<!-- Top Bar Start -->
+ <div id="top"><!-- Top Bar Start here-->
+ 	<div class="container"><!-- Container Start Here -->
+ 		<div class="row">
+ 			
+ 		
+ 		<div class="col-sm-12 col-md-6 offer"><!-- col-md-6 Start  here-->
+ 			<a href="#" class="btn btn-success btn-sm"> Welcome Guest</a>
+ 			<a href="#" class=""> Shooping Cart Total Rrice: INNR 100, Total Items 2</a>
+ 		</div><!-- col-md-6 end  here-->
+ 		<div class="col-sm-12 col-md-6 ">
+ 			<ul class="menu">
+ 				<li><a href="customer_resgister.php">Register</a></li>
+ 				<li><a href="checkout.php">Check Out</a></li>
+ 				<li><a href="cart.php">Go to Cart</a></li>
+ 				<li><a href="login.php">Login</a></li>
+ 				 
+ 			</ul>
+ 			
+ 		</div>
+
+</div>
+ 	</div><!-- Container end Here -->
+ </div><!-- Top Bar end here-->
+
+<div class="navbar navbar-default" id="nav"><!-- nav Bar start here-->
+	<div class="container">
+		<div class="navbar-header">
+			<a href="index.php" class="navbar-brand home">
+				<!-- <img src="images/logo.png" alt="Logo" class="hidden-xs"> -->
+				<img src="images/namkora3.jpg" alt="Logo" class="visible-xs">
+			</a>
+			
+		</div>
+		
+	</div>
+	
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/fontawesome.min.js"></script>
+</body>
+</html>
